5b644bed3f9a7ac1cf84c820d92ba636fb75cf12
-=fAlC0n=- <msaurabh2013@gmail.com>
2014-12-03 21:13:32 -0500

    Model files

---
+++ whatsInMyRefrigerator/LoginVC.h
@@ -0,0 +1,20 @@
+//
+//  LoginVC.h
+//  GroceFridge
+//
+//  Created by Student on 12/2/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITextField *usr;
+@property (nonatomic, strong) IBOutlet UITextField *pwd;
+
+-(IBAction)login:(id)sender;
+-(IBAction)signup:(id)sender;
+-(BOOL)checkpwd:(NSString *)usrid;
+
+@end
---
+++ whatsInMyRefrigerator/LoginVC.m
@@ -0,0 +1,37 @@
+//
+//  LoginVC.m
+//  GroceFridge
+//
+//  Created by Student on 12/2/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import "LoginVC.h"
+
+@interface LoginVC ()
+
+@end
+
+@implementation LoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
